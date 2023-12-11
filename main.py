e69318240c6acd959ad085f7171b716807b1a96b
Tom Krause <shermkaboom@gmail.com>
2023-12-11 13:05:57 +0100

    wokwi :)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import ADC, Pin, I2C
+from machine import ADC, Pin, SoftI2C
 from dht import DHT22
 from ssd1306 import SSD1306_I2C
 from time import sleep
@@ -6,19 +6,19 @@ import json
 
 sensor = DHT22(Pin(15))
 pir = Pin(2)
-photo = Pin(32)
+photo_sensor = Pin(32)
 led = Pin(33, Pin.OUT)
 led_red = Pin(25, Pin.OUT)
 led_yellow = Pin(26, Pin.OUT)
 led_green = Pin(27, Pin.OUT)
-i2c = I2C(scl=Pin(22), sda=Pin(21), freq=400000)
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=400000)
 oled = SSD1306_I2C(128, 64, i2c)
 
 
 def oled_show(temperature, humidity):
     oled.fill(0)
     oled.text("Temperature:", 10, 0)
-    oled.text(f"{temperature}°C", 10, 20)
+    oled.text(f"{temperature} C", 10, 20)
     oled.text(f"Humidity:", 10, 40)
     oled.text(f"{humidity}%", 10, 55)
     oled.show()
@@ -49,9 +49,15 @@ def sensor_measure_humidity():
     return sensor.humidity()
 
 
-def photo():
-    photo = ADC(Pin(32))
-    return photo.read()
+def photo_measure():
+    rl10 = 50
+    gamma = 0.7
+    dings = ADC(photo_sensor)
+    voltage = dings.read() / 4096 * 5
+    resistance = voltage * 2000 / (1 - voltage / 5)
+    lux = round(pow(rl10 * 1e3 * pow(10, gamma) / resistance, 1 / gamma), 0)
+
+    return lux
 
 
 def json_store(temperature, humidity, lux):
@@ -61,9 +67,7 @@ def json_store(temperature, humidity, lux):
         "lux": lux,
     }
 
-    # json export der nicht in wokwi funktioniert
-    # with open("data.json", "w") as f:
-    #    json.dump(data, f)
+    return json.dumps(data)
 
 
 def motion_sensor():
@@ -73,17 +77,20 @@ def motion_sensor():
         led.off()
 
 
-def print_to_terminal(temperature, humidity):
+def print_to_terminal(temperature, humidity, lux):
     print(f"Temperature: {temperature}C")
     print(f"Humidity: {humidity}%")
+    print(f"Lux: {lux}")
     print("-----------------------")
 
 
 while True:
     temperature_led()
-    json_store(sensor_measure_temperature(), sensor_measure_humidity(), photo())
+    json_store(sensor_measure_temperature(),
+               sensor_measure_humidity(), photo_measure())
     oled_show(sensor_measure_temperature(), sensor_measure_humidity())
     motion_sensor()
-    print_to_terminal(sensor_measure_temperature(), sensor_measure_humidity())
+    print_to_terminal(sensor_measure_temperature(),
+                      sensor_measure_humidity(), photo_measure())
 
     sleep(2)
